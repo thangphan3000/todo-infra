9050aa4f8e33d2975611b9fe79d11dd328816062
thangsuperman <thangjenny2002@gmail.com>
2025-01-29 14:24:07 +0700

    Add required provider version

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "non-prod-todo-terraform-state"
-    key    = "terraform"
-    region = "ap-southeast-1"
+    bucket         = "non-prod-todo-terraform-state"
+    key            = "terraform"
+    region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock-state"
   }
 }
