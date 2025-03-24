585aba4a65ee0f418c28eeab0f04a5dcef90d1ba
Thang Phan <thangjenny2002@gmail.com>
2025-03-24 11:48:39 +0700

    Refactor code

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -6,7 +6,7 @@ locals {
   }
 }
 
-data "aws_route53_zone" "root_domain" {
+data "aws_route53_zone" "public" {
   name = var.root_domain
 }
 
@@ -21,7 +21,7 @@ resource "aws_route53_zone" "private" {
 resource "aws_route53_record" "subdomain" {
   for_each = var.subdomains
   name     = "${var.environment}.${each.value.name}"
-  zone_id  = each.value.hosted_zone_type == "public" ? data.aws_route53_zone.root_domain.zone_id : aws_route53_zone.private.zone_id
+  zone_id  = each.value.hosted_zone_type == "public" ? data.aws_route53_zone.public.zone_id : aws_route53_zone.private.zone_id
   type     = each.value.record_type
   ttl      = each.value.ttl
   records  = [local.ips[each.value.ip]]
