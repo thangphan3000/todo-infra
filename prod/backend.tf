be0ae84a50dc965a1e54e6f4936d6bebbf839e63
Thang Phan <thangjenny2002@gmail.com>
2025-04-06 21:52:58 +0700

    clean: remove prod env

--- prod/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "non-prod-todo-terraform-state"
-    key            = "terraform"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-lock-state"
-  }
-}
